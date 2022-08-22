33dca4d3eed16740ca3a7b758209789478c9ce3e
pbouchaud <pbouchaud@gmail.com>
2022-08-22 13:12:48 +0800

    freeCNDB simplification

--- Base/entity.c
+++ Base/entity.c
@@ -9,59 +9,15 @@
 //===========================================================================
 //	cn_new
 //===========================================================================
-static void add_as_sub( CNEntity *, listItem **, const int pivot );
-
 CNEntity *
 cn_new( CNEntity *source, CNEntity *target )
 {
 	Pair *sub = newPair( source, target );
 	Pair *as_sub = newPair( NULL, NULL );
 	CNEntity *e = (CNEntity *) newPair( sub, as_sub );
-#ifndef OPT
 	if (( source )) addItem( &source->as_sub[0], e );
 	if (( target ))	addItem( &target->as_sub[1], e );
-#else
-	if (( source )) add_as_sub( e, &source->as_sub[0], 0 );
-	if (( target ))	add_as_sub( e, &target->as_sub[1], 1 );
-#endif
 	return e;
-}
-
-static void
-add_as_sub( CNEntity *e, listItem **as_sub, const int p )
-{
-	listItem *i, *last_i=NULL, *j=newItem( e );
-	switch ( p ) {
-	case 0:
-		e = e->sub[ 1 ];
-		for ( i=*as_sub; i!=NULL; i=i->next ) {
-			CNEntity *g = i->ptr;
-			intptr_t compare = (uintptr_t) g->sub[ 1 ] - (uintptr_t) e;
-			if ( compare < 0 ) { last_i=i; continue; }
-#ifdef DEBUG
-			else if ( !compare ) goto ERR;
-#endif
-			break;
-		}
-		break;
-	default:
-		e = e->sub[ 0 ];
-		for ( i=*as_sub; i!=NULL; i=i->next ) {
-			CNEntity *g = i->ptr;
-			intptr_t compare = (uintptr_t) g->sub[ 0 ] - (uintptr_t) e;
-			if ( compare < 0 ) { last_i=i; continue; }
-#ifdef DEBUG
-			else if ( !compare ) goto ERR;
-#endif
-			break;
-		}
-	}
-	if (( last_i )) { last_i->next = j; j->next = i; }
-	else { j->next = *as_sub; *as_sub = j; }
-	return;
-ERR:
-	fprintf( stderr, ">>>>> B%%: Error: add_as_sub(): already there\n" );
-	freeItem( j );
 }
 
 //===========================================================================
@@ -74,26 +30,14 @@ cn_instance( CNEntity *e, CNEntity *f, const int pivot )
 	case 0:
 		for ( listItem *i=f->as_sub[ 1 ]; i!=NULL; i=i->next ) {
 			CNEntity *instance = i->ptr;
-#ifndef OPT
 			if ( instance->sub[ 0 ] == e )
-#else
-			intptr_t comparison = (uintptr_t) instance->sub[ 0 ] - (uintptr_t) e;
-			if ( comparison < 0 ) continue;
-			else if ( comparison ) break;
-#endif
 				return instance;
 		}
 		break;
 	default:
 		for ( listItem *i=e->as_sub[ 0 ]; i!=NULL; i=i->next ) {
 			CNEntity *instance = i->ptr;
-#ifndef OPT
 			if ( instance->sub[ 1 ] == f )
-#else
-			intptr_t comparison = (uintptr_t) instance->sub[ 1 ] - (uintptr_t) f;
-			if ( comparison < 0 ) continue;
-			else if ( comparison ) break;
-#endif
 				return instance;
 		}
 	}
@@ -103,67 +47,53 @@ cn_instance( CNEntity *e, CNEntity *f, const int pivot )
 //===========================================================================
 //	cn_rewire
 //===========================================================================
-static void remove_as_sub( CNEntity *, listItem **, const int position );
-
 void
 cn_rewire( CNEntity *e, int ndx, CNEntity *sub )
+/*
+	Assumption: e->sub[ndx] is proper CNEntity *
+*/
 {
-	CNEntity *previous = e->sub[ ndx ];
-	remove_as_sub( e, &previous->as_sub[ ndx ], ndx );
+	CNEntity *previous = e->sub[ndx];
+	removeItem( &previous->as_sub[ndx], e );
+	addItem( &sub->as_sub[ ndx ], e );
 	e->sub[ ndx ] = sub;
-	add_as_sub( e, &sub->as_sub[ ndx ], ndx );
 }
 
-static void
-remove_as_sub( CNEntity *e, listItem **as_sub, const int p )
+//===========================================================================
+//	cn_prune
+//===========================================================================
+void
+cn_prune( CNEntity *e )
+/*
+	frees e and all relationship instances in %(( e, . ), ... ) form,
+	proceeding top-down
+*/
 {
-	CNEntity *h;
-	listItem *i, *last_i;
-	switch ( p ) {
-	case 0:
-#ifdef UNIFIED
-		if ( !(h=e->sub[ 1 ]) || !e->sub[0] ) {
-#else
-		if ( !(h=e->sub[ 1 ]) ) {
-#endif
-			removeItem( as_sub, e );
-			return;
+	listItem *stack = NULL;
+	listItem *i = newItem( e );
+	for ( ; ; ) {
+		listItem *j = e->as_sub[ 0 ];
+		if (( j )) {
+			addItem( &stack, i );
+			i = j; e = i->ptr;
+			continue;
 		}
-		for ( i=*as_sub, last_i=NULL; i!=NULL; i=i->next ) {
-			CNEntity *g = i->ptr;
-#ifdef UNIFIED
-			if ( !g->sub[0] ) { last_i=i; continue; }
-#endif
-			intptr_t compare = (uintptr_t) g->sub[ 1 ] - (uintptr_t) h;
-			if ( compare < 0 ) { last_i=i; continue; }
-			else if ( !compare ) {
-				if (( last_i )) last_i->next = i->next;
-				else *as_sub = i->next;
-				freeItem( i );
+		for ( ; ; ) {
+			cn_free( e );
+			if (( i->next )) {
+				i = i->next;
+				e = i->ptr;
+				break;
 			}
-			return;
-		}
-		break;
-	default:
-		if ( !(h=e->sub[ 0 ]) ) {
-			removeItem( as_sub, e );
-			return;
-		}
-		for ( i=*as_sub, last_i=NULL; i!=NULL; i=i->next ) {
-			CNEntity *g=i->ptr, *k=g->sub[ 0 ];
-#ifdef UNIFIED
-			if ( !k ) { last_i=i; continue; }
-#endif
-			intptr_t compare = (uintptr_t) k - (uintptr_t) h;
-			if ( compare < 0 ) { last_i=i; continue; }
-			else if ( !compare ) {
-				if (( last_i )) last_i->next = i->next;
-				else *as_sub = i->next;
-				freeItem( i );
+			else if (( stack )) {
+				i = popListItem( &stack );
+				e = i->ptr;
 			}
-			return;
+			else goto RETURN;
 		}
 	}
+RETURN:
+	freeItem( i );
 }
 
 //===========================================================================
@@ -177,17 +107,12 @@ cn_release( CNEntity *e )
 	3. frees e
 */
 {
-#ifndef UNIFIED
 	/* 1. remove e from the as_sub lists of its subs
 	*/
 	CNEntity *sub;
-#ifndef OPT
 	if (( sub=e->sub[0] )) removeItem( &sub->as_sub[0], e );
 	if (( sub=e->sub[1] )) removeItem( &sub->as_sub[1], e );
-#else // OPT
-	if (( sub=e->sub[0] )) remove_as_sub( e, &sub->as_sub[0], 0 );
-	if (( sub=e->sub[1] )) remove_as_sub( e, &sub->as_sub[1], 1 );
-#endif
+
 	/* 2. nullify e as sub of its as_sub's
 	*/
 	for ( listItem *j=e->as_sub[ 0 ]; j!=NULL; j=j->next ) {
@@ -202,74 +127,6 @@ cn_release( CNEntity *e )
 	*/
 	cn_free( e );
 
-#else // UNIFIED
-
-	/* 1. remove e from the as_sub lists of its subs
-	*/
-	CNEntity *sub;
-#ifndef OPT
-	if (( sub=e->sub[0] )) {
-		removeItem( &sub->as_sub[0], e );
-		if (( sub=e->sub[1] )) removeItem( &sub->as_sub[1], e );
-	}
-	else if (( sub = e->sub[1] )) {
-		Pair *pair = (Pair *) sub;
-		if (( pair->name )) removeItem( (listItem **) pair->name, e );
-		if (( pair->value )) removeItem( (listItem **) pair->value, e );
-		freePair( pair );
-	}
-#else // OPT
-	if (( sub=e->sub[0] )) {
-		remove_as_sub( e, &sub->as_sub[0], 0 );
-		if (( sub=e->sub[1] )) remove_as_sub( e, &sub->as_sub[1], 1 );
-	}
-	else if (( sub = e->sub[1] )) {
-		Pair *pair = (Pair *) sub;
-		if (( pair->name )) remove_as_sub( e, (listItem **) pair->name, 0 );
-		if (( pair->value )) remove_as_sub( e, (listItem **) pair->value, 1 );
-		freePair( pair );
-	}
-#endif
-	/* 2. nullify e as sub of its as_sub's
-	*/
-	for ( listItem *j=e->as_sub[ 0 ]; j!=NULL; j=j->next ) {
-		CNEntity *instance = j->ptr;
-		Pair *pair;
-		if (( instance->sub[0] )) {
-			sub = instance->sub[ 1 ];
-			pair = newPair( NULL, &sub->as_sub[1] );
-			instance->sub[0] = NULL;
-			instance->sub[1] = (CNEntity *) pair;
-		}
-		else if (( instance->sub[1] )) {
-			pair = (Pair *) instance->sub[ 1 ];
-			if ( pair->value == NULL ) {
-				freePair( pair );
-				instance->sub[1] = NULL;
-			}
-		}
-	}
-	for ( listItem *j=e->as_sub[ 1 ]; j!=NULL; j=j->next ) {
-		CNEntity *instance = j->ptr;
-		Pair *pair;
-		if (( instance->sub[0] )) {
-			sub = instance->sub[ 0 ];
-			pair = newPair( &sub->as_sub[0], NULL );
-			instance->sub[0] = NULL;
-			instance->sub[1] = (CNEntity *) pair;
-		}
-		else if (( instance->sub[1] )) {
-			pair = (Pair *) instance->sub[ 1 ];
-			if ( pair->name == NULL ) {
-				freePair( pair );
-				instance->sub[1] = NULL;
-			}
-		}
-	}
-	/* 3. free e
-	*/
-	cn_free( e );
-#endif
 }
 
 //===========================================================================
---
+++ Release/Version-WIP/database.c
@@ -0,0 +1,401 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "string_util.h"
+#include "database.h"
+#include "db_op.h"
+
+// #define DB_DEBUG
+#ifdef DB_DEBUG
+#include "db_debug.h"
+#define	db_op		db_op_debug
+#define	db_update	db_update_debug
+// #define TEST_INSTANTIATE
+// #define TEST_DEPRECATE
+#endif
+
+// #define DEBUG
+// #define NULL_TERMINATED
+
+//===========================================================================
+//	newCNDB
+//===========================================================================
+CNDB *
+newCNDB( void )
+{
+#ifdef NULL_TERMINATED
+	Registry *index = newRegistry( IndexedByName );
+#else
+	Registry *index = newRegistry( IndexedByCharacter );
+#endif
+	CNInstance *nil = cn_new( NULL, NULL );
+	nil->sub[ 0 ] = nil; // init condition
+	return (CNDB *) newPair( nil, index );
+}
+
+//===========================================================================
+//	freeCNDB
+//===========================================================================
+static freeRegistryCB free_CB;
+
+void
+freeCNDB( CNDB *db )
+/*
+	delete all CNDB entities, proceeding top down
+*/
+{
+	if ( db == NULL ) return;
+	cn_prune( db->nil );
+	freeRegistry( db->index, free_CB );
+	freePair((Pair *) db );
+}
+static void
+free_CB( Registry *registry, Pair *pair )
+{
+	free( pair->name );
+	cn_prune((CNinstance *) pair->value );
+}
+
+//===========================================================================
+//	db_register
+//===========================================================================
+CNInstance *
+db_register( char *p, CNDB *db, int manifest )
+/*
+	register instance represented by p
+	manifest only if requested AND if instance is new or rehabilitated
+*/
+{
+	if ( p == NULL ) return NULL;
+#ifdef NULL_TERMINATED
+	p = strmake( p );
+#endif
+	CNInstance *e = NULL;
+	Pair *entry = registryLookup( db->index, p );
+	if (( entry )) {
+		e = entry->value;
+		if ( manifest ) db_op( DB_REHABILITATE_OP, e, db );
+#ifdef NULL_TERMINATED
+		free( p );
+#endif
+	}
+	else {
+#ifndef NULL_TERMINATED
+		p = strmake( p );
+#endif
+		e = cn_new( NULL, NULL );
+#ifdef UNIFIED
+		e->sub[ 1 ] = (CNInstance *) p;
+#endif
+		registryRegister( db->index, p, e );
+		if ( manifest ) db_op( DB_MANIFEST_OP, e, db );
+	}
+	return e;
+}
+
+//===========================================================================
+//	db_couple
+//===========================================================================
+CNInstance *
+db_couple( CNInstance *e, CNInstance *f, CNDB *db )
+/*
+	Assumption: called upon CNDB creation - cf. cnLoad()/bm_substantiate()
+	Same as db_instantiate except without change registration
+*/
+{
+	CNInstance *instance;
+	if (( instance = cn_instance( e, f, 0 ) ))
+		return instance;
+	else if (( e->as_sub[ 0 ] )) {
+		Pair *star = registryLookup( db->index, "*" );
+		if (( star ) && ( e->sub[ 0 ] == star->value )) {
+			instance = e->as_sub[ 0 ]->ptr;
+			fprintf( stderr, "B%%::db_couple: Warning: reassigning " );
+			db_output( stderr, "", instance, db );
+			fprintf( stderr, " to " );
+			cn_rewire( instance, 1, f );
+			db_output( stderr, "", instance, db );
+			fprintf( stderr, "\n" );
+			return instance;
+		}
+	}
+	return cn_new( e, f );
+}
+
+//===========================================================================
+//	db_instantiate
+//===========================================================================
+CNInstance *
+db_instantiate( CNInstance *e, CNInstance *f, CNDB *db )
+/*
+	Assumption: neither e nor f are released
+	Special case: assignment - i.e. e is ( *, variable )
+		in which case we must deprecate current assignment
+	Note that until next frame, ((*,e), . ) will exist both as
+		the current (either to-be-released or reassigned) and
+		the new (either newborn, reassigned, or released-to-be-manifested)
+	assignments.
+*/
+{
+#ifdef DEBUG
+	fprintf( stderr, "db_instantiate: ( " );
+	db_output( stderr, "", e, db );
+	fprintf( stderr, ", " );
+	db_output( stderr, "", f, db );
+	fprintf( stderr, " )\n" );
+#endif
+#ifdef TEST_INSTANTIATE
+	test_instantiate( "db_instantiate( e, . )", e, db );
+	test_instantiate( "db_instantiate( ., f )", f, db );
+#endif
+	CNInstance *instance = NULL;
+	Pair *star = registryLookup( db->index, "*" );
+	if (( star ) && ( e->sub[ 0 ] == star->value )) {
+		/* Assignment case - as we have e:( *, . )
+		*/
+		for ( listItem *i=e->as_sub[0]; i!=NULL; i=i->next ) {
+			CNInstance *candidate = i->ptr;
+			if ( candidate->sub[ 1 ] == f ) {
+				instance = candidate;
+			}
+			else if ( db_deprecatable( candidate, db ) )
+				db_deprecate( candidate, db );
+		}
+		if (( instance )) {
+			db_op( DB_REASSIGN_OP, instance, db );
+			return instance;
+		}
+	}
+	else {
+		/* General case
+		*/
+		instance = cn_instance( e, f, 0 );
+		if (( instance )) {
+			db_op( DB_REHABILITATE_OP, instance, db );
+			return instance;
+		}
+	}
+	instance = cn_new( e, f );
+	db_op( DB_MANIFEST_OP, instance, db );
+	return instance;
+}
+
+//===========================================================================
+//	db_deprecate
+//===========================================================================
+void
+db_deprecate( CNInstance *x, CNDB *db )
+/*
+	deprecate (ie. set "to-be-released") x and all its ascendants,
+	proceeding top-down
+*/
+{
+	if ( x == NULL ) return;
+#ifdef TEST_DEPRECATE
+	test_deprecate( "db_deprecate()", x, db );
+#endif
+	listItem * stack = NULL,
+		 * i = newItem( x ),
+		 * j;
+	int ndx = 0;
+	for ( ; ; ) {
+		x = i->ptr;
+		for ( j=x->as_sub[ ndx ]; j!=NULL; j=j->next )
+			if ( db_deprecatable( j->ptr, db ) )
+				break;
+		if (( j )) {
+			addItem( &stack, i );
+			add_item( &stack, ndx );
+			ndx = 0;
+			i = j; continue;
+		}
+		for ( ; ; ) {
+			if ( ndx == 0 ) {
+				ndx = 1;
+				break;
+			}
+			if ((x)) {
+				db_op( DB_DEPRECATE_OP, x, db );
+			}
+			if (( i->next )) {
+				i = i->next;
+				if ( !db_deprecatable( i->ptr, db ) )
+					x = NULL;
+				else {
+					ndx = 0;
+					break;
+				}
+			}
+			else if (( stack )) {
+				ndx = (int) popListItem( &stack );
+				i = popListItem( &stack );
+				if ( ndx ) x = i->ptr;
+			}
+			else goto RETURN;
+		}
+	}
+RETURN:
+	freeItem( i );
+}
+
+//===========================================================================
+//	db_lookup
+//===========================================================================
+CNInstance *
+db_lookup( int privy, char *p, CNDB *db )
+{
+	if ( p == NULL )
+		return NULL;
+#ifdef NULL_TERMINATED
+	p = strmake( p );
+	Pair *entry = registryLookup( db->index, p );
+	free( p );
+#else
+	Pair *entry = registryLookup( db->index, p );
+#endif
+	return (( entry ) && !db_private( privy, entry->value, db )) ?
+		entry->value : NULL;
+}
+
+//===========================================================================
+//	db_identifier
+//===========================================================================
+char *
+db_identifier( CNInstance *e, CNDB *db )
+/*
+	Assumption: e->sub[0]==NULL already verified
+*/
+{
+#ifdef UNIFIED
+	return (char *) e->sub[ 1 ];
+#else
+	Pair *entry = registryLookup( db->index, NULL, e );
+	return ( entry ) ? entry->name : NULL;
+#endif
+}
+
+//===========================================================================
+//	db_is_empty
+//===========================================================================
+int
+db_is_empty( int privy, CNDB *db )
+{
+	for ( listItem *i=db->index->entries; i!=NULL; i=i->next ) {
+		Pair *entry = i->ptr;
+		if ( !db_private( privy, entry->value, db ) )
+			return 0;
+	}
+	return 1;
+}
+
+//===========================================================================
+//	db_first, db_next, db_traverse
+//===========================================================================
+CNInstance *
+db_first( CNDB *db, listItem **stack )
+{
+	for ( listItem *i=db->index->entries; i!=NULL; i=i->next ) {
+		Pair *entry = i->ptr;
+		CNInstance *e = entry->value;
+		if ( !db_private( 0, e, db ) ) {
+			addItem( stack, i );
+			return e;
+		}
+	}
+	return NULL;
+}
+CNInstance *
+db_next( CNDB *db, CNInstance *e, listItem **stack )
+{
+	if ( e == NULL ) return NULL;
+	if (( e->as_sub[ 0 ] )) {
+		for ( listItem *i=e->as_sub[0]; i!=NULL; i=i->next ) {
+			e = i->ptr;
+			if ( !db_private( 0, e, db ) ) {
+				addItem( stack, i );
+				return e;
+			}
+		}
+	}
+	listItem *i = popListItem( stack );
+	for ( ; ; ) {
+		if (( i->next )) {
+			i = i->next;
+			if (( *stack ))
+				e = i->ptr;
+			else {
+				// e in db->index
+				Pair *entry = i->ptr;
+				e = entry->value;
+			}
+			if ( !db_private( 0, e, db ) ) {
+				addItem( stack, i );
+				return e;
+			}
+		}
+		else if (( *stack )) {
+			i = popListItem( stack );
+		}
+		else return NULL;
+	}
+}
+int
+db_traverse( int privy, CNDB *db, DBTraverseCB user_CB, void *user_data )
+/*
+	traverses whole CNDB applying user_CB to each entity
+*/
+{
+	listItem *stack = NULL;
+	listItem *n = ( privy ? newItem( db->nil ) : NULL );
+	listItem *i = ( privy ? n : db->index->entries );
+	while (( i )) {
+		CNInstance *e;
+		if (( stack ) || (n))
+			e = i->ptr;
+		else {
+			Pair *pair = i->ptr;
+			e = pair->value;
+		}
+		listItem *j = e->as_sub[ 0 ];
+		if (( j )) {
+			addItem( &stack, i );
+			i = j; continue;
+		}
+		while (( i )) {
+			if ( user_CB( e, db, user_data ) ) {
+				freeListItem( &n );
+				freeListItem( &stack );
+				return 1;
+			}
+			if (( i->next )) {
+				i = i->next;
+				break;
+			}
+			else if (( stack )) {
+				i = popListItem( &stack );
+				if ((stack) || (n))
+					e = i->ptr;
+				else {
+					Pair *pair = i->ptr;
+					e = pair->value;
+				}
+			}
+			else if (( n )) {
+				freeListItem( &n );
+				i = db->index->entries;
+				break;
+			}
+			else i = NULL;
+		}
+	}
+	return 0;
+}
+
+//===========================================================================
+//	db_exit
+//===========================================================================
+void
+db_exit( CNDB *db )
+{
+	db_op( DB_EXIT_OP, NULL, db );
+}
