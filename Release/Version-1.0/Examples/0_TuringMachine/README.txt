1af2137dcc22470085a5b874e5f6f1f655ee5976
pbouchaud <pbouchaud@gmail.com>
2022-10-03 14:41:18 +0800

    Makefile & dist reorganization

---
+++ Release/Version-1.0/Examples/0_TuringMachine/README.txt
@@ -0,0 +1,22 @@
+Name
+	Consensus/Release/Version-1.0/Examples/0_TuringMachine/TM.story
+
+Usage
+	../../B% TM.story < Turing.Machine
+
+Description
+	Implements a Turing Machine[1] using the Consensus B% programming language.
+
+	This example features TM.story as the full B% program (aka. story in Consensus
+	terms) allowing to read in and execute a Turing Machine, thereby demonstrating
+	the Turing completeness of the language.
+
+	The Turing.Machine file features the machine from Lin and Rado which is given
+	as example[1].
+
+	The Test sub-directory features intermediate versions of the final TM.story.
+	The story Test/TM.4.verbose, akin to a 'debug' version, is intended to
+	facilitate understanding of the program.
+
+Reference
+	1. http://www.logique.jussieu.fr/~michel/tmi.html
--- Release/Version-1.0/main.c
+++ Release/Version-1.0/main.c
@@ -7,21 +7,44 @@
 
 // #define DEBUG
 
+void
+usage( void )
+{
+	fprintf( stderr, "B%%: Example Usage\n"
+		"\t./B%% file.story\n"
+		"\t./B%% -p file.story\n"
+	);
+	exit(-1);
+}
+
 int
 main( int argc, char *argv[] )
 {
-	if ( argc < 2 ) {
-		fprintf( stderr, "Example usage: ./B%% Test/CN.suite\n" );
-		exit( - 1 );
+	// interprete B% command args
+
+	if ( argc < 2 ) usage();
+
+	char *path = NULL;
+	int printout = 0;
+	if ( !strncmp( argv[1], "-p", 2 ) ) {
+		if ( argc < 3 ) usage();
+		path = argv[ 2 ];
+		printout = 1;
 	}
+	else {
+		if ( argc < 2 ) usage();
+		path = argv[ 1 ];
+	}
+
+	// execute B% command
+
 	CNDB *db = newCNDB();
-	CNNarrative *narrative = readNarrative( argv[ 1 ] );
-#ifdef DEBUG
-	narrative_output( stderr, narrative, 0 );
-	exit( 0 );
-#endif
-	while ( cnOperate( narrative, db ) )
-		cnUpdate( db );
+	CNNarrative *narrative = readNarrative( path );
+	if ( printout )
+		narrative_output( stdout, narrative, 0 );
+	else
+		while ( cnOperate( narrative, db ) )
+			cnUpdate( db );
 
 	freeNarrative( narrative );
 	freeCNDB( db );
